2153f82e861f378b023acad4a039d29dc2f387bf
AZEFUAD <fuadkhs@code.edu.az>
2023-03-07 21:27:06 +0400

    Your file successfully finished.

---
+++ PharmacyAppFS/Data/DbInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Core.Entities;
+using Core.Helpers;
+using Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public static class DbInitializer
+    {
+        static int id;
+        public static void SeedAdmins()
+        {
+            var admins = new List<Admin>
+            {
+                new Admin
+                {
+                    Id = ++id,
+                    Username = "admin01",
+                    Password = PasswordHasher.Encrypt("244844"),
+                    CreatedBy = "Created by System"
+                },
+                new Admin
+                {
+                    Id = ++id,
+                    Username = "admin02",
+                    Password = PasswordHasher.Encrypt("367298"),
+                    CreatedBy = "Created by System"
+                },
+                new Admin
+                {
+                    Id = ++id,
+                    Username = "admin03",
+                    Password = PasswordHasher.Encrypt("145678"),
+                    CreatedBy = "Created by System"
+                }
+            };
+            DbContext.Admins.AddRange(admins);
+        }
+    }
+}
